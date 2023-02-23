86cad2d6870d7e811ee52a5463797543a09c8cd4
Rachel-oc <mengting.xie@thoughtworks.com>
2023-02-23 14:57:43 +0800

    [feat]: add the play again button

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -24,6 +24,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.ScoreFragmentBinding
 
@@ -44,6 +45,7 @@ class ScoreFragment : Fragment() {
             container,
             false
         )
+        binding.playAgainButton.setOnClickListener { viewModel.onPlayAgain() }
         viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(requireArguments()).score)
         viewModel = ViewModelProvider(this, viewModelFactory)
             .get(ScoreViewModel::class.java)
@@ -51,6 +53,15 @@ class ScoreFragment : Fragment() {
             viewLifecycleOwner,
             Observer { newScore ->
                 binding.scoreText.text = newScore.toString()
+            }
+        )
+        viewModel.eventPlayAgain.observe(
+            viewLifecycleOwner,
+            Observer { playAgain ->
+                if (playAgain) {
+                    NavHostFragment.findNavController(this).navigate(ScoreFragmentDirections.actionRestart())
+                    viewModel.onPlayAgainComplete()
+                }
             }
         )
         return binding.root
